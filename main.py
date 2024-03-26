edd678d9b500bd2d2164c7e40a43eb2fbdf7cc8f
Developer9844 <d3692094@gmail.com>
2024-03-26 13:05:08 +0530

    second small change

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def hello_world():
 <html>
 <body>
 <center>
-<h1>Demo on GitOps with ArgoCD and Github Actions.</h1> <br>
+<h1>Demo-1 on GitOps with ArgoCD and Github Actions.</h1> <br>
 <br>
 <img src="https://github.com/tanmaybhandge/CICD_Application_K8s/blob/main/itsworking.jpeg?raw=true">
 <p>Hello My name is Developer9844</p>
