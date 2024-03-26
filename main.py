12a5255a7f0f1475e024e80b92841059dd452f43
Developer9844 <d3692094@gmail.com>
2024-03-26 15:05:07 +0530

    123

--- main.py
+++ main.py
@@ -14,11 +14,7 @@ def hello_world():
 together with local recovery. If the working directory is configured to reside on a persistent volume that gets <br>
 remounted to a restarted TaskManager pod, then Flink is able to recover state locally. With the StatefulSet, <br>
 Kubernetes gives you the exact tool you need to map a pod to a persistent volume.
-
-Deploying TaskManagers as a StatefulSet, allows you to configure a volume <br>
-claim template that is used to mount persistent volumes to the TaskManagers. Additionally, you need <br>
-to configure a deterministic taskmanager.resource-id. A suitable value is the pod name, that you expose using <br>
-environment variables. For an example StatefulSet configuration take a look at the appendix.</p>
+</p>
 </center>
 </body>
 </html>
